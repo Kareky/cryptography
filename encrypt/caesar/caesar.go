b7e895597bcb209c07a0402fe9367f228873388b
Kareky <riccardo.morosin@hotmail.com>
2026-05-14 16:25:14 +0200

    Generalize Caesar cipher with Alphabet interface; remove case preservation

---
+++ encrypt/caesar/caesar.go
@@ -0,0 +1,28 @@
+package caesar
+
+import (
+	"strings"
+	"unicode"
+	"github.com/Kareky/cryptography/internal/alphabet"
+)
+
+// Encrypt takes a message and encrypts it, moving each character inside the sentence
+// according to shift. a determines the alphabet used for the encryption.
+// The function returns the shifted string, always lowercase,
+// preserving each space or symbol as it was in the original message.
+func Encrypt(text string, shift int, a alphabet.Alphabet) string {
+	var newText strings.Builder
+	for _, character := range text {
+		character = unicode.ToLower(character)
+		newText.WriteRune(alphabet.ShiftLetter(character, shift, a))
+	}
+	return newText.String()
+}
+
+// Decrypt takes an encrypted message and decrypts it using a shift.
+// a determines the alphabet used for the encryption.
+// The function returns the shifted string, always lowercase,
+// preserving each space or symbol as it was in the original message.
+func Decrypt(text string, shift int, a alphabet.Alphabet) string {
+	return Encrypt(text, -shift, a)
+}
--- encrypt/caesar/caesar_test.go
+++ encrypt/caesar/caesar_test.go
@@ -3,59 +3,60 @@ package caesar_test
 import (
 	"testing"
 	"github.com/Kareky/cryptography/encrypt/caesar"
+	"github.com/Kareky/cryptography/internal/alphabet"
 )
 
 func TestEncrypt(t *testing.T) {
 	type args struct {
-		text                string
-		shift               int
-		preserveUppercase   bool
+		text        string
+		shift      	int
+		Alphabet	alphabet.Alphabet
 	}
 	tests := []struct {
-		name string
-		args args
-		want string
+		name 		string
+		args 		args
+		want 		string
 	}{
 		{
-			name: "Encrypt with shift 3 and preserve uppercase",
+			name: "Encrypt with shift 3",
 			args: args{
-				text:              "Hello, World!",
-				shift:             3,
-				preserveUppercase: true,
+				text:       "Hello, World!",
+				shift:      3,
+				Alphabet: 	alphabet.LatinAlphabet,
 			},
-			want: "Khoor, Zruog!",
+			want: "khoor, zruog!",
 		},
 		{
-			name: "Encrypt with shift 5 and do not preserve uppercase",
+			name: "Encrypt with shift 5",
 			args: args{
-				text:              "Hello, World!",
-				shift:             5,
-				preserveUppercase: false,
+				text:       "Hello, World!",
+				shift:      5,
+				Alphabet: 	alphabet.LatinAlphabet,
 			},
 			want: "mjqqt, btwqi!",
 		},
 		{
-			name: "Encrypt with shift 0 and preserve uppercase",
+			name: "Encrypt with shift 0",
 			args: args{
-				text:              "Hello, World!",
-				shift:             0,
-				preserveUppercase: true,
+				text:       "Hello, World!",
+				shift:      0,
+				Alphabet: 	alphabet.LatinAlphabet,
 			},
-			want: "Hello, World!",
+			want: "hello, world!",
 		},
 		{
-			name: "Encrypt with shift -3 and preserve uppercase",
+			name: "Encrypt with shift -3",
 			args: args{
-				text:              "Hello, World!",
-				shift:             -3,
-				preserveUppercase: true,
+				text:       "Hello, World!",
+				shift:      -3,
+				Alphabet: 	alphabet.LatinAlphabet,
 			},
-			want: "Ebiil, Tloia!",
+			want: "ebiil, tloia!",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := caesar.Encrypt(tt.args.text, tt.args.shift, tt.args.preserveUppercase); got != tt.want {
+			if got := caesar.Encrypt(tt.args.text, tt.args.shift, tt.args.Alphabet); got != tt.want {
 				t.Errorf("Encrypt() = %v, want %v", got, tt.want)
 			}
 		})
@@ -64,9 +65,9 @@ func TestEncrypt(t *testing.T) {
 
 func TestDecrypt(t *testing.T) {
 	type args struct {
-		text                string
-		shift               int
-		preserveUppercase   bool
+		text        string
+		shift      	int
+		Alphabet	alphabet.Alphabet
 	}
 	tests := []struct {
 		name string
@@ -74,45 +75,45 @@ func TestDecrypt(t *testing.T) {
 		want string
 	}{
 		{
-			name: "Decrypt with shift 3 and preserve uppercase",
+			name: "Decrypt with shift 3",
 			args: args{
-				text:              "Khoor, Zruog!",
-				shift:             3,
-				preserveUppercase: true,
-			},
-			want: "Hello, World!",
-		},
-		{
-			name: "Decrypt with shift 5 and do not preserve uppercase",
-			args: args{
-				text:              "mjqqt, btwqi!",
-				shift:             5,
-				preserveUppercase: false,
+				text:       "Khoor, Zruog!",
+				shift:      3,
+				Alphabet: 	alphabet.LatinAlphabet,
 			},
 			want: "hello, world!",
 		},
 		{
-			name: "Decrypt with shift 0 and preserve uppercase",
+			name: "Decrypt with shift 5",
 			args: args{
-				text:              "Hello, World!",
-				shift:             0,
-				preserveUppercase: true,
+				text:       "mjqqt, btwqi!",
+				shift:      5,
+				Alphabet: 	alphabet.LatinAlphabet,
 			},
-			want: "Hello, World!",
+			want: "hello, world!",
 		},
 		{
-			name: "Decrypt with shift -3 and preserve uppercase",
+			name: "Decrypt with shift 0",
 			args: args{
-				text:              "Ebiil, Tloia!",
-				shift:             -3,
-				preserveUppercase: true,
+				text:       "Hello, World!",
+				shift:      0,
+				Alphabet: 	alphabet.LatinAlphabet,
 			},
-			want: "Hello, World!",
+			want: "hello, world!",
+		},
+		{
+			name: "Decrypt with shift -3",
+			args: args{
+				text:       "Ebiil, Tloia!",
+				shift:      -3,
+				Alphabet: 	alphabet.LatinAlphabet,
+			},
+			want: "hello, world!",
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := caesar.Decrypt(tt.args.text, tt.args.shift, tt.args.preserveUppercase); got != tt.want {
+			if got := caesar.Decrypt(tt.args.text, tt.args.shift, tt.args.Alphabet); got != tt.want {
 				t.Errorf("Decrypt() = %v, want %v", got, tt.want)
 			}
 		})
