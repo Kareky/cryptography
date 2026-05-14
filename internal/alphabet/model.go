77f82e01f13dd8a7bd81e75ca83c41102a6577f1
Kareky <riccardo.morosin@hotmail.com>
2026-05-14 15:09:59 +0200

    Clean up Alphabet interface: remove Map method, rename Alphabet→ToSlice, add LatinAlphabet

--- internal/alphabet/model.go
+++ internal/alphabet/model.go
@@ -4,37 +4,25 @@ type Alphabet interface {
 	Len() int                   	//length of the alphabet
 	RuneFor(pos int) rune       	// the rune at position pos in the alphabet
 	Position(r rune) (int, bool)	// the position of the rune in the alphabet
-	MapOrdered() map[rune]int    	// a map of rune and their position
-	MapEmpty() map[rune]int			// a map of runes with 0 value, normally used to count characters
-	Alphabet() []rune           	// the whole alphabet
+	ToSlice() []rune           		// return the alphabet as a slice
 	Name() string               	// the name of the alphabet
 }
 
 type Latin []rune
 
-func (l Latin) Len() int             { return len(l) }
-func (l Latin) RuneFor(pos int) rune { return l[pos] }
-func (l Latin) Name() string         { return "latin" }
-func (l Latin) Alphabet() []rune     { return []rune("abcdefghijklmnopqrstuvwxyz") }
-func (l Latin) MapOrdered() map[rune]int {
-	alph := make(map[rune]int)
-	for i, character := range l.Alphabet() {
-		alph[character] = i
-	}
+var LatinAlphabet = Latin([]rune("abcdefghijklmnopqrstuvwxyz"))
 
-	return alph
-}
-
-func (l Latin) MapEmpty() map[rune]int {
-	alph := make(map[rune]int)
-	for _, character := range l.Alphabet() {
-		alph[character] = 0
-	}
-
-	return alph
-}
+func (l Latin) Len() int             	{ return len(l) }
+func (l Latin) RuneFor(pos int) rune 	{ return l[pos] }
+func (l Latin) Name() string         	{ return "latin" }
+func (l Latin) ToSlice() []rune     	{ return LatinAlphabet }
 
 func (l Latin) Position(r rune) (int, bool) {
-	pos, ok := l.MapOrdered()[r]
-	return pos, ok
+	for i, char := range l {
+		if char == r {
+			return i, true
+		}
+	}
+
+	return 0, false
 }
