764cc28ad5a09fe5f601ca83e25fc3febd051707
Kareky <riccardo.morosin@hotmail.com>
2026-05-15 18:21:33 +0200

    Created a version of encryption which remove all symbols and spaces

--- encrypt/caesar/caesar.go
+++ encrypt/caesar/caesar.go
@@ -11,6 +11,21 @@ import (
 // The function returns the shifted string, always lowercase,
 // preserving each space or symbol as it was in the original message.
 func Encrypt(text string, shift int, a alphabet.Alphabet) string {
+	return encrypt(text, shift, a, true)
+}
+
+// EncryptStripped takes a message and encrypts it, moving each character inside the sentence
+// according to shift. a determines the alphabet used for the encryption.
+// The function returns the shifted string, always lowercase,
+// with all spaces and symbols removed.
+func EncryptStripped(text string, shift int, a alphabet.Alphabet) string {
+	return encrypt(text, shift, a, false)
+}
+
+// encrypt contains the core logic of encryption as described in Encrypt and EncryptStripped.
+// preserveFormatting is a boolean that decide if formatting will be kept during characters shifting,
+// or if they will be removed.
+func encrypt(text string, shift int, a alphabet.Alphabet, preserveFormatting bool) string {
 	var encryptedStr strings.Builder
 	for _, char := range text {
 		char = unicode.ToLower(char)
--- encrypt/polyalphabetic/polyalphabetic.go
+++ encrypt/polyalphabetic/polyalphabetic.go
@@ -11,10 +11,29 @@ import (
 // of the key word passed.
 // a determines the alphabet used for the cipher.
 // The function returns the encrypted string, always lowercase, preserving each space or symbol
-// as it was in the original message.
+// as it was in the original message. If the intent is to remove them, call EncryptStripped instead.
 // It returns an error if encryptWord contains non-letters or letters that don't exist in the chosen alphabet.
 // If encryptWord is empty, the text is returned unchanged.
 func Encrypt(text, encryptWord string, a alphabet.Alphabet) (string, error) {
+	return encrypt(text, encryptWord, a, true)
+}
+
+// EncryptStripped takes a message and encrypts it, moving each character inside the sentence
+// according to a cycle of shifts determined by the position in the alphabet of the letters
+// of the key word passed.
+// a determines the alphabet used for the cipher.
+// The function returns the encrypted string, always lowercase, with all spaces and symbols removed.
+// If the intent is to preserve them, call Encrypt instead
+// It returns an error if encryptWord contains non-letters or letters that don't exist in the chosen alphabet.
+// If encryptWord is empty, the text is returned unchanged.
+func EncryptStripped(text, encryptWord string, a alphabet.Alphabet) (string, error) {
+	return encrypt(text, encryptWord, a, false)
+}
+
+// encrypt contains the core logic of encryption as described in Encrypt and EncryptStripped.
+// preserveFormatting is a boolean that decide if formatting will be kept during characters shifting,
+// or if they will be removed.
+func encrypt(text, encryptWord string, a alphabet.Alphabet, preserveFormatting bool) (string, error) {
 	if len(encryptWord) == 0 {
 		return text, nil
 	}
@@ -32,8 +51,11 @@ func Encrypt(text, encryptWord string, a alphabet.Alphabet) (string, error) {
 		if _, ok := a.Position(char); ok {
 			char = alphabet.ShiftLetter(char, encryptSeq[i % len(encryptSeq)], a)
 			i++
+			encryptedStr.WriteRune(char)
+		} else if preserveFormatting {
+			encryptedStr.WriteRune(char)
 		}
-		encryptedStr.WriteRune(char)
+		
 	}
 
 	return encryptedStr.String(), nil
